51aea4e61af1926907f5321265613bbf7649182e
Konstantin <dem.konst@yandex.ru>
2022-01-10 19:01:05 +0500

    Section 5 finished

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -3,6 +3,9 @@ import express, { json } from 'express';
 const app = express();
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!');
+});
 
 app.listen(3000, () => {
   console.log('Auth is listening on port 3000');
