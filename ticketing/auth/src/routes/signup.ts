f893709ea2630ee3242dbf463bd01d4a44a9ffa4
Konstantin Demianenko <dem.konst@yandex.ru>
2022-10-29 13:35:33 +0300

    L-140: files refactoring

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -12,7 +12,7 @@ router.post('/api/users/signup', [
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      throw new Error('Invalid email or password');
+      return res.status(400).send(errors.array());
     }
 
     const { email, password } = req.body;
