4593bf42b065faeeef2a6c8fd76b3c72389c67df
Konstantin Demianenko <dem.konst@yandex.ru>
2022-12-20 14:59:04 +0300

    L-153: Connecting to MongoDB

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -21,6 +22,20 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Auth is listening on port 3000');
-});
+const start = async () => {
+  mongoose.set('strictQuery', false);
+
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB');
+
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log('Auth is listening on port 3000');
+  });
+};
+
+start();
